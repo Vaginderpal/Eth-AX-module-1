d17079815f8ab43289cbe8cf7aa6c65b3156879e
Vaginderpal Singh Brar <137271458+Vaginderpal@users.noreply.github.com>
2023-07-09 08:54:11 +0530

    ErrorHandling.sol

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract ourcontract {
-    uint public _num = 5;
-         
+contract Error {
+    uint public num = 2;
 
-    function testassert(uint _newNum) public pure  {
-        assert(_newNum != 0);
-        // assert function 
+    function testRequire(uint _i) public pure {
+        // Require should be used to validate conditions such as:
+        // - conditions before execution
+        require(_i > 10, "Input must be greater than 10");
     }
 
-    function testRevert(uint _newNum) public pure {
-        if (_newNum == 10) {
-            revert("num cannot be 10");
+    function testRevert(uint _i) public pure {
+        // Revert is useful when the condition to check is complex.
+        // This code does the exact same thing as the example above
+        if (_i <= 5) {
+            revert("Input not be smaller than 5");
         }
     }
 
-    function testrequire() public view {
-        require(_num > 0, "num cannot be 0");
+    
+
+    function testAssert() public view {
+        // Assert should only be used to test for internal errors,
+        // Here we assert that num is always equal to 0
+    
+        assert(num != 0);
     }
-}
